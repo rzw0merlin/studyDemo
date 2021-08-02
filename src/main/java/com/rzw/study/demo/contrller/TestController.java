96be7764294d3918363cb5d2026a3f1f1d5683e9
“502795506@qq.com” <renzhiwei@ideatech.info>
2021-08-02 16:37:23 +0800

    测试

---
+++ src/main/java/com/rzw/study/demo/contrller/TestController.java
@@ -0,0 +1,10 @@
+package com.rzw.study.demo.contrller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    public void study(){
+
+    }
+}
